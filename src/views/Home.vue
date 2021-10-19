42af6feb1bd42d6cee1ef358ea32cdbd4c8b231e
Ahmane Samir <samirahmane.trading@gmail.com>
2021-10-19 08:43:56 +0100

    feat: formatting

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,77 +1,79 @@
 <template>
   <div class="home">
     <section class="hero is-medium mb-6">
-        <div class="hero-body has-text-centered">
-            <p class="title mb-6 is-1 welcome-font">
-                Bienvenue à Cosfluencer
-            </p>
-            <p class="subtitle welcome-font">
-                Créez votre pack pour votre communauté
-            </p>
-        </div>
+      <div class="hero-body has-text-centered">
+        <p class="title mb-6 is-1 welcome-font">
+          Bienvenue à Cosfluencer
+        </p>
+        <p class="subtitle welcome-font">
+          Créez votre pack pour votre communauté
+        </p>
+      </div>
     </section>
 
     <div class="columns is-multiline">
       <div class="column is-12">
-          <h2 class="is-size-2 has-text-centered welcome-font">Nos Derniers Produits</h2>
+        <h2 class="is-size-2 has-text-centered welcome-font">
+          Nos Derniers Produits
+        </h2>
       </div>
 
-      <ProductBox 
+      <ProductBox
         v-for="product in latestProducts"
         v-bind:key="product.id"
-        v-bind:product="product" />
+        v-bind:product="product"
+      />
     </div>
   </div>
 </template>
 
 <script>
-import axios from 'axios'
+import axios from "axios";
 
-import ProductBox from '@/components/ProductBox'
+import ProductBox from "@/components/ProductBox";
 
 export default {
-  name: 'Home',
+  name: "Home",
   data() {
     return {
-      latestProducts: []
-    }
+      latestProducts: [],
+    };
   },
   components: {
-    ProductBox
+    ProductBox,
   },
   mounted() {
-    this.getLatestProducts()
+    this.getLatestProducts();
 
-    document.title = 'Home | Djackets'
+    document.title = "Home | Djackets";
   },
   methods: {
     async getLatestProducts() {
-      this.$store.commit('setIsLoading', true)
+      this.$store.commit("setIsLoading", true);
 
       await axios
-        .get('/api/v1/latest-products/')
-        .then(response => {
-          this.latestProducts = response.data
+        .get("/api/v1/latest-products/")
+        .then((response) => {
+          this.latestProducts = response.data;
         })
-        .catch(error => {
-          console.log(error)
-        })
+        .catch((error) => {
+          console.log(error);
+        });
 
-      this.$store.commit('setIsLoading', false)
-    }
-  }
-}
+      this.$store.commit("setIsLoading", false);
+    },
+  },
+};
 </script>
 
 <style style="scss">
-@import url('https://fonts.googleapis.com/css?family=Ephesis');
-.welcome-font{
-  font-family: 'Ephesis', sans-serif;
+@import url("https://fonts.googleapis.com/css?family=Ephesis");
+.welcome-font {
+  font-family: "Ephesis", sans-serif;
 }
-.bg-img { 
-    background-image: url("../assets/home.jpg");
-    background-repeat:  no-repeat;
-    background-size: 2000px 352px;
+.bg-img {
+  background-image: url("../assets/home.jpg");
+  background-repeat: no-repeat;
+  background-size: 2000px 352px;
 }
-
 </style>
