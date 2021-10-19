183d9325c95b018880cfa840791017a7bba0fd77
Ahmane Samir <samirahmane.trading@gmail.com>
2021-10-19 08:42:46 +0100

    feat: new state store is adapted to cart and language changed

--- src/components/CartItem.vue
+++ src/components/CartItem.vue
@@ -1,54 +1,61 @@
 <template>
-    <tr class="welcome-font">
-        <td><router-link :to="item.product.get_absolute_url">{{ item.product.name }}</router-link></td>
-        <td>${{ item.product.price }}</td>
-        <td>
-            {{ item.quantity }}
-            <a @click="decrementQuantity(item)">-</a>
-            <a @click="incrementQuantity(item)">+</a>
-        </td>
-        <td>${{ getItemTotal(item).toFixed(2) }}</td>
-        <td><button class="delete" @click="removeFromCart(item)"></button></td>
-    </tr>
+  <tr class="welcome-font">
+    <td>
+      <router-link :to="item.product.get_absolute_url">{{
+        item.product.name
+      }}</router-link>
+    </td>
+    <td class="font-king is-size-5">{{ item.product.price }}DZD</td>
+    <td>
+      {{ item.quantity }}
+      <a class="font-king is-size-5" @click="decrementQuantity(item)">-</a>
+      <a class="font-king is-size-5" @click="incrementQuantity(item)">+</a>
+    </td>
+    <td class="font-king is-size-5">{{ getItemTotal(item).toFixed(2) }}DZD</td>
+    <td class="">
+      <button class="delete is-danger" @click="removeFromCart(item)"></button>
+    </td>
+  </tr>
 </template>
 
 <script>
 export default {
-    name: 'CartItem',
-    props: {
-        initialItem: Object
+  name: "CartItem",
+  props: {
+    initialItem: Object,
+  },
+  data() {
+    return {
+      item: this.initialItem,
+    };
+  },
+  methods: {
+    getItemTotal(item) {
+      return item.quantity * item.product.price;
     },
-    data() {
-        return {
-            item: this.initialItem
-        }
+    decrementQuantity(item) {
+      item.quantity -= 1;
+
+      if (item.quantity === 0) {
+        this.$emit("removeFromCart", item);
+      }
+
+      this.updateCart();
+      this.$store.state.totalCart -= 1;
     },
-    methods: {
-        getItemTotal(item) {
-            return item.quantity * item.product.price
-        },
-        decrementQuantity(item) {
-            item.quantity -= 1
-
-            if (item.quantity === 0) {
-                this.$emit('removeFromCart', item)
-            }
-
-            this.updateCart()
-        },
-        incrementQuantity(item) {
-            item.quantity += 1
-
-            this.updateCart()
-        },
-        updateCart() {
-            localStorage.setItem('cart', JSON.stringify(this.$store.state.cart))
-        },
-        removeFromCart(item) {
-            this.$emit('removeFromCart', item)
-
-            this.updateCart()
-        },
+    incrementQuantity(item) {
+      item.quantity += 1;
+      this.$store.state.totalCart += 1;
+      this.updateCart();
     },
-}
+    updateCart() {
+      localStorage.setItem("cart", JSON.stringify(this.$store.state.cart));
+    },
+    removeFromCart(item) {
+      this.$emit("removeFromCart", item);
+      this.$store.state.totalCart = this.$store.state.totalCart - item.quantity;
+      this.updateCart();
+    },
+  },
+};
 </script>
